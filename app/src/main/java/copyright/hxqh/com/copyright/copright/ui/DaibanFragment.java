17ccd8cd4e7927fe7197ad96289c51dc4a03e20f
lianjy <825858433qq@.com>
2018-10-19 17:12:14 +0800

    a

--- app/src/main/java/copyright/hxqh/com/copyright/copright/ui/DaibanFragment.java
+++ app/src/main/java/copyright/hxqh/com/copyright/copright/ui/DaibanFragment.java
@@ -1,8 +1,10 @@
 package copyright.hxqh.com.copyright.copright.ui;
 
 import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.app.Fragment;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.widget.SwipeRefreshLayout;
@@ -15,10 +17,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.chad.library.adapter.base.BaseQuickAdapter;
@@ -27,15 +33,22 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import copyright.hxqh.com.copyright.R;
 import copyright.hxqh.com.copyright.copright.HttpManager.HttpConnect;
+import copyright.hxqh.com.copyright.copright.adpter.DaibanAdapter;
+import copyright.hxqh.com.copyright.copright.adpter.SpinnerArrayAdapter;
 import copyright.hxqh.com.copyright.copright.ui.publicService.LawVinDicateDetailActivity;
 import copyright.hxqh.com.copyright.copright.ui.publicService.RoyaltyActivity;
 import copyright.hxqh.com.copyright.copright.ui.publicService.adapter.LawvindicateAdapter;
+import copyright.hxqh.com.copyright.copright.ui.publicService.entity.Consult;
 import copyright.hxqh.com.copyright.copright.util.AcountUtil;
+import copyright.hxqh.com.copyright.copright.util.CumTimePickerDialog;
 
 /**
  * Created by lianjh on 2018\10\18 0018.
@@ -61,11 +74,27 @@ public class DaibanFragment extends Fragment implements SwipeRefreshLayout.OnRef
     @Bind(R.id.swipe_container)
     SwipeRefreshLayout refresh_layout;//界面刷新
 
+    TextView startdate;
+    TextView enddate;
+    Spinner spinner;
+
     private int page = 1;
     private int pageNum;
 
     private JSONObject json;
 
+    DaibanAdapter daibanAdapter;
+
+    /**
+     * 时间控件
+     */
+    private DatePickerDialog datePickerDialog;
+
+    private int layoutnum;
+    StringBuffer sb;
+
+    private ArrayAdapter<String> mAdapter ;
+    private String [] mStringArray;
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
@@ -102,7 +131,7 @@ public class DaibanFragment extends Fragment implements SwipeRefreshLayout.OnRef
         searchButton.setImageResource(R.drawable.ic_handlight);
         json = HttpConnect.getBasicJson(getActivity());
         initAdpter();
-//        AcountUtil.showProgressDialog(getActivity(), "");
+        AcountUtil.showProgressDialog(getActivity(), "");
         getData();
         searchButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -112,13 +141,24 @@ public class DaibanFragment extends Fragment implements SwipeRefreshLayout.OnRef
 
                 Button btn_cancel_high_opion = view.findViewById(R.id.btn_cancel_high_opion);
                 Button btn_agree_high_opion = view.findViewById(R.id.btn_agree_high_opion);
-                TextView startdate = view.findViewById(R.id.startdate_id);
-                TextView enddate = view.findViewById(R.id.enddate_id);
+                startdate = view.findViewById(R.id.startdate_id);
+                enddate = view.findViewById(R.id.enddate_id);
+                spinner = view.findViewById(R.id.spinner2);
+                mStringArray=getResources().getStringArray(R.array.spingarr);
+                //使用自定义的ArrayAdapter
+                mAdapter = new SpinnerArrayAdapter(getActivity(),mStringArray);
+                spinner.setAdapter(mAdapter);
+                //监听Item选中事件
+                spinner.setOnItemSelectedListener(new ItemSelectedListenerImpl());
 
                 btn_cancel_high_opion.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        dialog.dismiss();
+                        startdate.setText("");
+                        enddate.setText("");
+                        startdate.setHint("请选择时间");
+                        enddate.setHint("请选择时间");
+                        spinner.setSelection(0);
                     }
                 });
 
@@ -129,25 +169,71 @@ public class DaibanFragment extends Fragment implements SwipeRefreshLayout.OnRef
                         dialog.dismiss();
                     }
                 });
-                startdate.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-
-                    }
-                });
-                enddate.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-
-                    }
-                });
+                setDataListener();
+                startdate.setOnClickListener(new MydateListener());
+                enddate.setOnClickListener(new MydateListener());
 
                 dialog.show();
 
             }
         });
     }
+    /**
+     * 下拉选中的条目*
+     */
+    private class ItemSelectedListenerImpl implements AdapterView.OnItemSelectedListener {
+        @Override
+        public void onItemSelected(AdapterView<?> parent, View view, int position,long arg3) {
+            String flowType = mStringArray[position];
+            System.out.println("选中了:"+flowType);
+        }
 
+        @Override
+        public void onNothingSelected(AdapterView<?> parent) {}
+
+    }
+
+    /**
+     * 设置时间选择器*
+     */
+    private void setDataListener() {
+        final Calendar objTime = Calendar.getInstance();
+        int iYear = objTime.get(Calendar.YEAR);
+        int iMonth = objTime.get(Calendar.MONTH);
+        int iDay = objTime.get(Calendar.DAY_OF_MONTH);
+
+        datePickerDialog = new DatePickerDialog(getActivity(), new datelistener(), iYear, iMonth, iDay);
+    }
+    private class MydateListener implements View.OnClickListener {
+
+        @Override
+        public void onClick(View view) {
+            layoutnum = 0;
+            sb = new StringBuffer();
+            layoutnum = view.getId();
+            datePickerDialog.show();
+        }
+    }
+
+    private class datelistener implements DatePickerDialog.OnDateSetListener {
+
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+            sb = new StringBuffer();
+            monthOfYear = monthOfYear + 1;
+            if (dayOfMonth < 10) {
+                sb.append(year + "-" + monthOfYear + "-" + "0" + dayOfMonth);
+            } else {
+                sb.append(year + "-" + monthOfYear + "-" + dayOfMonth);
+            }
+
+            if (layoutnum == startdate.getId()) {
+                startdate.setText(sb);
+            }else if (layoutnum == enddate.getId()){
+                enddate.setText(sb);
+            }
+        }
+    }
     @Override
     public void onRefresh() {
         refresh_layout.setRefreshing(true);
@@ -157,17 +243,73 @@ public class DaibanFragment extends Fragment implements SwipeRefreshLayout.OnRef
     }
 
     private void getData() {
+        new AsyncTask<String, String, List<Consult>>() {
+
+            @Override
+            protected List<Consult> doInBackground(String... strings) {
+                List<Consult> products = (List<Consult>) HttpConnect.getConsultlist(json, page);
+                return products;
+            }
+
+            @Override
+            protected void onPostExecute(List<Consult> preoducts) {
+                super.onPostExecute(preoducts);
+                if (preoducts == null) {
+                    daibanAdapter.loadMoreFail();
+                    if (daibanAdapter.getData().isEmpty()) {
+                        AcountUtil.closeProgressDialog();
+                        nodatalayout.setVisibility(View.VISIBLE);
+                        TextView textView = (TextView) nodatalayout.getChildAt(0);
+                        textView.setText("加载失败请检查网络");
+                    }
+                    return;
+                }
+                if (preoducts.isEmpty()) {
+                    if (daibanAdapter.getData().isEmpty()) {
+                        nodatalayout.setVisibility(View.VISIBLE);
+                        TextView textView = (TextView) nodatalayout.getChildAt(0);
+                        textView.setText("暂无数据");
+                        daibanAdapter.replaceData(new ArrayList<Consult>());
+                    } else {
+                        daibanAdapter.loadMoreEnd();
+                    }
+                } else {
+                    pageNum = preoducts.get(0).getPagenum();
+                    nodatalayout.setVisibility(View.GONE);
+                    if (daibanAdapter.getData().isEmpty()) {
+                        daibanAdapter.addData(preoducts);
+                    } else {
+                        if (page == 1) {
+                            daibanAdapter.setNewData(preoducts);
+                        } else if (page <= pageNum){
+                            daibanAdapter.addData(preoducts);
+                            daibanAdapter.loadMoreComplete();
+                        }else {
+                            daibanAdapter.loadMoreEnd();
+                        }
+                    }
+                }
+                daibanAdapter.setEnableLoadMore(true);
+                refresh_layout.setRefreshing(false);
+                AcountUtil.closeProgressDialog();
+            }
+
+            @Override
+            protected void finalize() throws Throwable {
+                super.finalize();
+            }
+        }.execute();
     }
 
     private void initAdpter() {
-//        lawvindicateAdapter = new LawvindicateAdapter(R.layout.activity_daiban_item);
-//        recyclerView.setAdapter(lawvindicateAdapter);
-//        lawvindicateAdapter.setOnLoadMoreListener(new BaseQuickAdapter.RequestLoadMoreListener() {
-//            @Override
-//            public void onLoadMoreRequested() {
-//                page++;
-//                getData();
-//            }
-//        }, recyclerView);
+        daibanAdapter = new DaibanAdapter(R.layout.activity_daiban_item,getActivity());
+        recyclerView.setAdapter(daibanAdapter);
+        daibanAdapter.setOnLoadMoreListener(new BaseQuickAdapter.RequestLoadMoreListener() {
+            @Override
+            public void onLoadMoreRequested() {
+                page++;
+                getData();
+            }
+        }, recyclerView);
     }
 }
